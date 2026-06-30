e73a7408dd43a94f7d2944ec1aa655f88499da39
RIVERSOFT <105160096+RIVERS0FT@users.noreply.github.com>
2026-06-30 11:43:01 +0800

    Add profile route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { LanguageProvider } from "./context/LanguageContext";
 import { HomePage } from "./pages/HomePage";
 import { AuthPage } from "./pages/AuthPage";
+import { ProfilePage } from "./pages/ProfilePage";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<AuthPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
